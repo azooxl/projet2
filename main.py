901f35a71c8692460a4b2e1d3329a5fac6d983e3
azooxl <mssazox@gmail.com>
2023-01-30 16:50:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from random import randint
-x = randint(1,10)
+import os 
+x = 4
 y = x
 tableau = [[randint(0,1) for k in range(x)]for i in range(y)]
 
@@ -10,5 +11,26 @@ def aff():
 def cel(x, y, tableau):
     return tableau[y][x]
 
-aff()
+# aff()
+
+a = 1
+a = int(a)
+for i in range(len(tableau)):
+    for j in range(len(tableau[i])):
+        (tableau[i][j - 1] == 1)
+        (tableau[i][j + 1] == 1)
+        (tableau[i + 1][j + 1] == 1)
+        (tableau[i + 1][j - 1] == 1)
+        (tableau[i - 1][j + 1] == 1)
+        (tableau[i - 1][j - 1] == 1)
+        (tableau[i - 1][j] == 1)
+        (tableau[i + 1][j] == 1)
+        
+
+#clear = lambda: os.system('cls')
+#clear()
+
+
+
+
 
